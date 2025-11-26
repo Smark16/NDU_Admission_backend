2436cb83dbc4fde7f8eb1c8b729fef60b967d3a4
Smark16 <sengendomark16@gmail.com>
2025-11-26 10:07:36 +0300

    cloudinary

--- ndu_portal/settings.py
+++ ndu_portal/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from decouple import config
 from datetime import timedelta
 from .env import BASE_DIR, env
-import cloudinary
+# import cloudinary
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 env.read_env(os.path.join(BASE_DIR, '.env'))
