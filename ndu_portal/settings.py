a990a0f83d42cfea0280299881c96805726874da
Smark16 <sengendomark16@gmail.com>
2026-04-17 12:48:19 +0300

    comma

--- ndu_portal/settings.py
+++ ndu_portal/settings.py
@@ -246,7 +246,7 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(hours=4),
     "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,,
+    "BLACKLIST_AFTER_ROTATION": True,
 
     "ALGORITHM": "HS256",  # Google uses RS256, not HS256
     "SIGNING_KEY": SECRET_KEY,
