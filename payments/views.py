399079f2717c0760653707e096a035d755dcb39f
Smark16 <sengendomark16@gmail.com>
2026-04-18 03:34:46 +0300

    ggg

--- payments/views.py
+++ payments/views.py
@@ -83,7 +83,7 @@ class InitiatePayment(APIView):
         amount = request.data.get('amount')
         reason = "Application Fee"
         callBackUrl = f"{settings.BACKEND_URL}/api/payments/webhook/"
-        # callBackUrl = "https://97be-196-43-131-1.ngrok-free.app/api/payments/webhook/" 
+        # callBackUrl = "https://3e09-41-75-181-144.ngrok-free.app/api/payments/webhook/" 
 
         # EXPIRE OLD PAYMENTS
         ApplicationPayment.objects.filter(
