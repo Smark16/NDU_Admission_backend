a471fd4cfd9de0667010279841c526475a0f5150
Smark16 <sengendomark16@gmail.com>
2026-04-29 10:37:12 +0300

    fee

--- payments/admin.py
+++ payments/admin.py
@@ -6,3 +6,7 @@ class PaymentAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'external_reference', 'payment_reference', 'transaction_id', 'status']
     search_fields = ['user__first_name', 'user__last_name', 'external_reference', 'payment_reference', 'transaction_id']
     list_filter = ['status', 'created_at', 'updated_at']
+    
+@admin.register(ApplicationFee)
+class ApplicationFeeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'fee_type', 'nationality_type', 'amount', 'admission_period']
