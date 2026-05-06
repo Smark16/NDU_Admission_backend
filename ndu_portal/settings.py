a92976194b87a37efc45ba01ffdd204ccd520b6f
Smark16 <sengendomark16@gmail.com>
2026-05-07 02:56:30 +0300

    efgdhedhgef

--- ndu_portal/settings.py
+++ ndu_portal/settings.py
@@ -55,9 +55,9 @@ LOCAL_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'easyaudit.middleware.easyaudit.EasyAuditMiddleware',
