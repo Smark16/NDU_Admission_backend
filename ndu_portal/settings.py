bbf615ac36de488e48489402727c7e93381a8972
Smark16 <sengendomark16@gmail.com>
2026-05-07 12:42:59 +0300

    ddffff

--- ndu_portal/settings.py
+++ ndu_portal/settings.py
@@ -251,6 +251,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://applications-admin.ndu.ac.ug",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://admissions.ndu.ac.ug"
 ]
 
 # # Also allow subdomains if needed
