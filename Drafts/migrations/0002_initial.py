10b7675164370ed0a271a50badd63e99cb0fc125
Smark16 <sengendomark16@gmail.com>
2026-05-07 18:28:53 +0300

    fffffffnew

--- Drafts/migrations/0002_initial.py
+++ Drafts/migrations/0002_initial.py
@@ -26,7 +26,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='draftapplication',
             name='applicant',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='drafts', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='drafts', to=settings.AUTH_USER_MODEL, null=True, blank=True),
         ),
         migrations.AddField(
             model_name='draftapplication',
