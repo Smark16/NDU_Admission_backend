6c9c57766f3f07cbe46aab30326a54e130476d6e
Smark16 <sengendomark16@gmail.com>
2026-05-13 10:15:30 +0300

    frfrfrfr

--- Drafts/models.py
+++ Drafts/models.py
@@ -27,7 +27,7 @@ class DraftApplication(models.Model):
 
     # Next of Kin
     nextOfKinName = models.CharField(max_length=200, blank=True, null=True)
-    next_of_kin_contact = models.CharField(max_length=20, blank=True, null=True)
+    next_of_kin_contact = models.CharField(max_length=25, blank=True, null=True)
     next_of_kin_relationship = models.CharField(max_length=20, blank=True, null=True)
 
     # Program & Campus
